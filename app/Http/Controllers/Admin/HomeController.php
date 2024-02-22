0f2bf97e77a6a0daf8bc4d19c40b761b01874d12
kirill <kiriffuuu@gmail.com>
2024-02-22 17:47:06 +0600

    На 5 уроке

---
+++ app/Http/Controllers/Admin/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class HomeController extends Controller
+{
+    public function index()
+    {     
+        // return view('home');
+
+        $posts_count = Post::all()->count();
+
+        return view('admin.home.index', [
+            'posts_count' => $posts_count
+        ]);
+    }
+}
