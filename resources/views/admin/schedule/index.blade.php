4b4c942834bbcf2ba62d14eabe5a6d4a4ba3b286
kirill <kiriffuuu@gmail.com>
2024-02-27 10:41:29 +0600

    Все работает норм, до поправок Ивана

--- resources/views/admin/schedule/index.blade.php
+++ resources/views/admin/schedule/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Расписание')
 @section('content')
 
-<style>
+    {{-- <style>
     .fc-timeGridWeek-view {
         font-size: 12px;
         line-height: 10px;
@@ -35,132 +35,156 @@
         width: 90% !important;
         /* Ширина 90% от ячейки */
     }
-</style>
+</style> --}}
 
-<script>
-    // var jsonEventSources =
-    //     '[{"events":[{"title":"event1","start":"2024-02-25"},{"title":"event2","start":"2024-02-26","end":"2024-02-27"},{"title":"event3","start":"2024-02-26T12:30:00"}],"color":"black","textColor":"yellow"}]';
-    // var eventSources = JSON.parse(jsonEventSources);
-    var eventSources = [{
-        events: [{
-                title: 'История Казахстана',
-                start: '2024-02-25T12:30:00',
-                end: '2024-02-25T13:00:00'
-            },
-            {
-                title: 'Автоматизация бухгалтерского учета в 1С: Предприятие',
-                start: '2024-02-27T12:30:00',
-                end: '2024-02-27T13:00:00'
-            },
-            {
-                title: 'Клиент-серверные технологии',
-                start: '2024-02-26T12:30:00',
-                end: '2024-02-26T13:00:00'
-            },
-            {
-                title: 'Клиент-серверные технологии',
-                start: '2024-02-26T13:20:00',
-                end: '2024-02-26T14:00:00',
-
-            }
-        ],
-    }];
-</script>
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        var calendarEl = document.getElementById('calendar');
-
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-            locale: 'ru',
-            headerToolbar: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-            },
-            buttonText: {
-                today: 'Сегодня',
-                month: 'Месяц',
-                week: 'Неделя',
-                day: 'День',
-                list: 'Список'
-            },
-            eventContent: function(arg) {
-                return {
-                    html: '<div class="custom-event">' + arg.event.title + '</div>',
-                    classNames: ['custom-event']
-                };
-            },
-            eventDisplay: 'block',
-            views: {
-                dayGrid: {
-                    // Параметры, применяемые к представлениям dayGridMonth, dayGridWeek и dayGridDay
-                    dayMaxEventRows: 1, // Максимальное количество строк событий в ячейке дня
-                    dayMaxEvents: true // Ограничивать количество событий, отображаемых в ячейке дня
+    <script>
+        // var jsonEventSources =
+        //     '[{"events":[{"title":"event1","start":"2024-02-25"},{"title":"event2","start":"2024-02-26","end":"2024-02-27"},{"title":"event3","start":"2024-02-26T12:30:00"}],"color":"black","textColor":"yellow"}]';
+        // var eventSources = JSON.parse(jsonEventSources);
+        var eventSources = [{
+            events: [{
+                    title: 'История Казахстана',
+                    start: '2024-02-25T12:30:00',
+                    end: '2024-02-25T13:00:00'
                 },
-                timeGrid: {
-                    // Параметры, применяемые к представлениям timeGridWeek и timeGridDay
-                    slotDuration: '00:15:00', // Длительность временного слота
-                    slotLabelFormat: {
-                        hour: 'numeric',
-                        minute: '2-digit',
-                        hour12: false
-                    } // Формат меток времени слотов
+                {
+                    title: 'Автоматизация бухгалтерского учета в 1С: Предприятие',
+                    start: '2024-02-27T12:30:00',
+                    end: '2024-02-27T13:00:00'
                 },
-
-                timeGridWeek: {
-
-
+                {
+                    title: 'Клиент-серверные технологии',
+                    start: '2024-02-26T12:30:00',
+                    end: '2024-02-26T13:00:00'
                 },
+                {
+                    title: 'Клиент-серверные технологии',
+                    start: '2024-02-26T13:20:00',
+                    end: '2024-02-26T14:00:00',
 
-                day: {
-                    // Параметры, применяемые к представлениям dayGridDay и timeGridDay
-                    nowIndicator: true // Отображать индикатор текущего времени
                 }
-            },
-            eventSources: eventSources,
-            eventClick: function(info) {
-                calendar.changeView('timeGridDay');
+            ],
+        }];
+    </script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                locale: 'ru',
+                slotDuration: '00:30:00',
+                slotMinTime: '08:30:00',
+                slotMaxTime: '23:30:00',
+                eventDisplay: 'block',
+                firstDay: 1,
+                // aspectRatio: 2,
+
+
+                headerToolbar: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+                },
+                buttonText: {
+                    today: 'Сегодня',
+                    month: 'Месяц',
+                    week: 'Неделя',
+                    day: 'День',
+                    list: 'Список'
+                },
+                eventContent: function(arg) {
+                    return {
+                        html: '<div class="custom-event">' + arg.event.title + '</div>',
+                        classNames: ['custom-event']
+                    };
+                },
+                // views: {
+                //     dayGrid: {
+                //         dayMaxEventRows: 1, // Максимальное количество строк событий в ячейке дня
+                //         dayMaxEvents: true // Ограничивать количество событий, отображаемых в ячейке дня
+                //     },
+                //     timeGrid: {
+
+
+
+                //     },
+
+                //     timeGridWeek: {
+                //         slotLabelInterval: '01:00',
+
+
+                //     },
+
+                //     day: {
+                //         // Параметры, применяемые к представлениям dayGridDay и timeGridDay
+                //         // nowIndicator: true // Отображать индикатор текущего времени
+                //     }
+                // },
+                eventSources: eventSources,
+                eventClick: function(info) {
+                    calendar.changeView('timeGridDay');
+                },
+
+            });
+
+            // Устанавливаем пользовательское отображение событий
+            calendar.setOption('eventContent', function(arg) {
+                var currentView = calendar.view.type;
+                if (currentView === 'timeGridWeek') {
+                    return formatEventContent(arg);
+                } else {
+                    return arg.event.title;
+                }
+            });
+
+            // Вспомогательная функция для форматирования содержимого события для режима timeGridWeek
+            function formatEventContent(arg) {
+                var title = arg.event.title;
+                if (title.length > 20) {
+                    title = title.substring(0, 20) + '...';
+                }
+                return {
+                    html: '</div><div style="font-size: 10px">' + title + '</div>'
+                };
             }
+            calendar.render();
+
         });
+    </script>
 
-        calendar.render();
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Расписание</h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
 
-    });
-</script>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="card">
+                <div class="card-body p-0">
+                    <div id="calendar">
 
-<!-- Content Header (Page header) -->
-<div class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0">Расписание</h1>
-            </div><!-- /.col -->
-        </div><!-- /.row -->
-    </div><!-- /.container-fluid -->
-</div>
-<!-- /.content-header -->
+                    </div>
 
-<!-- Main content -->
-<section class="content">
-    <div class="container-fluid">
-        <div class="card">
-            <div class="card-body p-0">
-                <div id="calendar">
-
+                    <!-- <div id="calendar-buttons">
+                                                                <button id="btn-week">Неделя 1</button>
+                                                                <button id="btn-week2">Неделя 2</button>
+                                                                <button id="btn-week3">Неделя 3</button>
+                                                            </div> -->
                 </div>
-
-                <!-- <div id="calendar-buttons">
-                        <button id="btn-week">Неделя 1</button>
-                        <button id="btn-week2">Неделя 2</button>
-                        <button id="btn-week3">Неделя 3</button>
-                    </div> -->
+                <!-- /.card-body -->
             </div>
-            <!-- /.card-body -->
-        </div>
-    </div><!-- /.container-fluid -->
-</section>
+        </div><!-- /.container-fluid -->
+    </section>
 
 
-<!-- /.content -->
+    <!-- /.content -->
 
 @endsection
