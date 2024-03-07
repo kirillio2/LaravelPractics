7753593bdadb62043b9ca48123ae68bdd17ae28c
kirill <kiriffuuu@gmail.com>
2024-03-07 16:23:00 +0500

    Все работает норм, +

--- resources/views/admin/schedule/index.blade.php
+++ resources/views/admin/schedule/index.blade.php
@@ -11,18 +11,14 @@
         .card-primary {
             width: 200px;
         }
-
-        .card-header {
+    </style>
+    {{-- .card-header {
             padding: 0.1rem 1.25rem;
         }
 
         .card-body {
             padding: 0.2rem 1.25rem;
-        }
-
-
-        
-    </style>
+        } --}}
     <script>
         $('body').delegate("#dropdownMenuButton1", "click", function() {
             var dropdownMenu = $(this).next('.card-body');
@@ -60,7 +56,6 @@
                 $('#uniqueCardBody2').css('display', 'block');
             }
         });
-
     </script>
 
     <script>
@@ -97,7 +92,8 @@
 
                 eventContent: function(arg) {
                     return {
-                        html: '<div class="custom-event">' + arg.event.title + '<span class="float-right badge bg-danger">842</span> ап</div>',
+                        html: '<div class="custom-event">' + arg.event.title +
+                            '<span class="float-right badge bg-danger">842</span> ап</div>',
                         classNames: ['custom-event']
                     };
                 },
@@ -119,36 +115,39 @@
                     $('.popover').remove();
 
                     var popoverBody = '<b>Время: </b>' + event.extendedProps.data.time + '</br>' +
-                                    '<b>Кабинет: </b>' + event.extendedProps.data.cabinet  + '</br>' +
-                                    '<b>Преподователь:</b>' +
-                                    '<div class="card-header">' +
-                                    '<h4 class="card-title" style="font-size: 0.99rem; color: red;">' + event.extendedProps.data.nameTeacher  + '</h4>' +
-                                    '<div class="card-tools">' +
-                                    '<a role="button" id="dropdownMenuButton1" class="btn btn-tool">' +
-                                    '<i class="fas fa-plus"></i>' +
-                                    '</a>' +
-                                    '</div>' +
-                                    '</div>' +
-                                    '<div class="card-body" id="uniqueCardBody1">' +
-                                    'Контакты: '+ event.extendedProps.data.phoneTeacher + ', </br>' + 
-                                    'Почта: ' + event.extendedProps.data.mailTeacher + '</br>' +
-                                    '</div>' +
-                                    '</div>' +
+                        '<b>Кабинет: </b>' + event.extendedProps.data.cabinet + '</br>' +
+                        '<b>Преподователь:</b>' +
+                        '<div class="card-header">' +
+                        '<h4 class="card-title" style="font-size: 0.99rem; color: red;">' + event
+                        .extendedProps.data.nameTeacher + '</h4>' +
+                        '<div class="card-tools">' +
+                        '<a role="button" id="dropdownMenuButton1" class="btn btn-tool">' +
+                        '<i class="fas fa-plus"></i>' +
+                        '</a>' +
+                        '</div>' +
+                        '</div>' +
+                        '<div class="card-body" id="uniqueCardBody1">' +
+                        '<b>Контакты: </b>' + event.extendedProps.data.phoneTeacher + ', </br>' +
+                        '<b>Почта: </b>' + event.extendedProps.data.mailTeacher + '</br>' +
+                        '</div>' +
+                        '</div>' +
 
-                                    '<b>Материалы:</b>' +
-                                    '<div class="card-header">' +
-                                    '<h4 class="card-title" style="font-size: 0.99rem; color: red;">ДОТ</h4>' +
-                                    '<div class="card-tools">' +
-                                    '<a role="button" id="dropdownMenuButton2" class="btn btn-tool">' +
-                                    '<i class="fas fa-plus"></i>' +
-                                    '</a>' +
-                                    '</div>' +
-                                    '</div>' +
-                                    '<div class="card-body" id="uniqueCardBody2">' +
-                                    '<a href="'+ event.extendedProps.data.fileMaterial + '">PDF </a>Справочник путеводить 2023+pdf</br>' + 
-                                    '<a href="'+ event.extendedProps.data.ecordingLecture + '">Запись </a>Острянина Т.К 19.02 10.30</br>' + 
-                                    '</div>' +
-                                    '</div>' ;
+                        '<b>Материалы:</b>' +
+                        '<div class="card-header">' +
+                        '<h4 class="card-title" style="font-size: 0.99rem; color: red;">ДОТ</h4>' +
+                        '<div class="card-tools">' +
+                        '<a role="button" id="dropdownMenuButton2" class="btn btn-tool">' +
+                        '<i class="fas fa-plus"></i>' +
+                        '</a>' +
+                        '</div>' +
+                        '</div>' +
+                        '<div class="card-body" id="uniqueCardBody2">' +
+                        '<a href="' + event.extendedProps.data.fileMaterial +
+                        '">PDF </a>Справочник путеводить 2023+pdf</br>' +
+                        '<a href="' + event.extendedProps.data.ecordingLecture +
+                        '">Запись </a>Острянина Т.К 19.02 10.30</br>' +
+                        '</div>' +
+                        '</div>';
 
                     $(document).ready(function() {
                         $('#uniqueCardBody1').css('display', 'none');
@@ -197,8 +196,8 @@
                     if (tooltip) {
                         tooltip.remove();
                     }
-                }, 
-                
+                },
+
 
             });
 
@@ -218,60 +217,162 @@
                 var currentView = calendar.view.type;
                 if (currentView === 'timeGridWeek') {
                     return formatEventContentTimeGridWeek(arg);
+                } else if (currentView === 'dayGridMonth') {
+                    return formatEventContentTimeGridMonth(arg);
+                } else if (currentView === 'timeGridDay') {
+                    return formatEventContentTimeGridDay(arg);
+                } else if (currentView === 'listWeek') {
+                    return formatEventContentTimeGridList(arg);
                 } else {
                     return formatEventContent(arg);
                 }
             });
 
-            // Вспомогательная функция для форматирования содержимого события для режима timeGridWeek
+
+
             function formatEventContentTimeGridWeek(arg) {
                 var title = arg.event.title;
-                if (title.length > 20) {
-                    title = title.substring(0, 20) + '...';
-                }
+                // if (title.length > 20) {
+                //     title = title.substring(0, 20) + '...';
+                // }
                 return {
-                    html: '</div><div style="font-size: 10px">' + title + '<span class="float-right badge bg-success">NEW</span></div>'
+                    html: '</div><div style="position: relative; font-size: 10px; z-index: 1;">' + title +
+                        '<span class="float-left badge bg-success" style="position: absolute; top: -10px; left: 10px; z-index: 2;">NEW</span></div>'
                 };
+
             }
 
-            // Вспомогательная функция для форматирования содержимого события для всех кроме timeGridWeek
             function formatEventContent(arg) {
                 var title = arg.event.title;
                 return {
-                    html: title + '<span class="float-right badge bg-success">NEW</span></div>'
+                    html: '</div><div style="position: relative; font-size: 15px; z-index: 1;">' + title +
+                        '<span class="float-left badge bg-success" style="position: absolute; top: -10px; left: 0; z-index: 2;">NEW</span></div>'
                 };
+
             }
-            
+
+            function formatEventContentTimeGridMonth(arg) {
+                var title = arg.event.title;
+                return {
+                    html: '</div><div style="position: relative; font-size: 15px; z-index: 1;">' + title +
+                        '<span class="float-left badge bg-success" style="position: absolute; top: -15px; left: 10px; z-index: 2;">NEW</span></div>'
+                };
+
+            }
+
+            function formatEventContentTimeGridDay(arg) {
+                var title = arg.event.title;
+                return {
+                    html: '</div><div style="position: relative; font-size: 15px; z-index: 1;">' + title +
+                        '<span class="float-left badge bg-success" style="position: absolute; top: -15px; left: 10px; z-index: 2;">NEW</span></div>'
+                };
+
+            }
+
+            function formatEventContentTimeGridList(arg) {
+                var title = arg.event.title;
+                return {
+                    html: '</div><div style="position: relative; font-size: 15px; z-index: 1;">' + title +
+                        '<span class="float-left badge bg-success" style="position: absolute; top: -15px; left: 10px; z-index: 2;">NEW</span></div>'
+                };
+
+            }
+
             calendar.render();
-
-
-            
 
         });
     </script>
 
-    <!-- Content Header (Page header) -->
     <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
                     <h1 class="m-0">Расписание</h1>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
+                </div>
+            </div>
+        </div>
     </div>
-    <!-- /.content-header -->
 
-    <!-- Main content -->
     <section class="content">
         <div class="container-fluid">
             <div class="card">
                 <div class="card-body p-0">
                     <div id="calendar"></div>
                 </div>
-                <!-- /.card-body -->
             </div>
-        </div><!-- /.container-fluid -->
+        </div>
     </section>
 
+
+
+    <table id="weeklyData" class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Дисциплины</th>
+                <th>Дата</th>
+                <th>Оценка</th>
+                <!-- Добавьте остальные заголовки столбцов -->
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td rowspan="3">Дисциплина 1</td>
+                <td>2024-01-15 - 2024-01-21</td>
+                <td>4.5</td>
+                <!-- Добавьте оценки для Дисциплины 1 -->
+            </tr>
+            <tr>
+                <td>2024-01-22 - 2024-01-28</td>
+                <td>4.0</td>
+                <!-- Добавьте оценки для Дисциплины 1 -->
+            </tr>
+            <tr>
+                <td>2024-01-29 - 2024-02-04</td>
+                <td>3.8</td>
+                <!-- Добавьте оценки для Дисциплины 1 -->
+            </tr>
+            <tr>
+                <td rowspan="3">Дисциплина 2</td>
+                <td>2024-01-15 - 2024-01-21</td>
+                <td>3.7</td>
+                <!-- Добавьте оценки для Дисциплины 2 -->
+            </tr>
+            <tr>
+                <td>2024-01-22 - 2024-01-28</td>
+                <td>4.2</td>
+                <!-- Добавьте оценки для Дисциплины 2 -->
+            </tr>
+            <tr>
+                <td>2024-01-29 - 2024-02-04</td>
+                <td>4.0</td>
+                <!-- Добавьте оценки для Дисциплины 2 -->
+            </tr>
+            
+            <!-- Добавьте остальные строки с данными для других дисциплин -->
+        </tbody>
+    </table>
+    
+    
+    
+    
+
+
+    <script>
+        $(document).ready(function() {
+            $('#weeklyData').DataTable({
+                "columnDefs": [{
+                    "render": function(data, type, row) {
+                        return moment(data).format('YY-MM-DD');
+                    },
+                    "targets": 1
+                }],
+                "order": [[1, "asc"]],
+                "paging": true, // Включаем пагинацию
+                "stripeClasses": ['odd', 'even'] // Устанавливаем классы для чередования строк
+            });
+        });
+    </script>
+    
+    
+    
 @endsection
